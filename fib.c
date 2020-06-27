5054022f5facb03e2b69f67b934de7c6df96e810
Seohyun Jang <63863258+D1N0-pixel@users.noreply.github.com>
2020-06-27 22:22:22 +0900

    Update fib.c

--- fib.c
+++ fib.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-int main(a,b,c,tmp1,tmp2)
+int main(a, b, c, tmp1, tmp2)
 {
-    if(scanf("%d",&a)){}
-    if(tmp1=1){}
-    if(tmp2=1){}
-    if(c=a%2){}
-    if(b=(a-c)/2){}
-    while(b-->0){
-        if(printf("%d %d ",tmp1,tmp2)){}
-        if(tmp1=tmp1+tmp2){}
-        if(tmp2=tmp1+tmp2){}
+    if (scanf("%d", &a)){}
+    if (tmp1 = 1){}
+    if (tmp2 = 1){}
+    if (c = a % 2){}
+    if (b = (a - c) / 2){}
+    while (b-- > 0)
+    {
+        if (printf("%d %d ", tmp1, tmp2)){}
+        if (tmp1 = tmp1 + tmp2){}
+        if (tmp2 = tmp1 + tmp2){}
     }
-    if(c)
-        if(printf("%d\n",tmp1)){}
+    if (c)
+        if (printf("%d\n", tmp1)){}
 }
